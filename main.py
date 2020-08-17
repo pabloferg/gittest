d6d6eb33c81d7681df01cee5c0ba3b5313ad88b6
Pablo Fernandez <pablofergui@gmail.com>
2020-08-17 16:03:29 +0100

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 print("Hello World")
+
+print("Hello Pablo")
+
+# $ git add .
+# $ git status # Lists all new or modified files to be committed
+# $ git commit -m "Second commit"
+# $ git push -u origin master
